0ef2e24a3f32f33e51fc11887b5c1ff80957badf
Pao-17 <pgur1712@gmail.com>
2025-03-03 15:14:44 -0500

    final

--- LAMBDAS/FT.py
+++ LAMBDAS/FT.py
@@ -1,44 +1,85 @@
 import json
 import math
+import boto3
 
-def carga_signal_json(json_string):
+client = boto3.client('lambda')
+
+def generar_tiempo(fs, duracion):
+    paso = 1/fs
+    num_muestras = math.ceil(duracion / paso)
+    tiempo = [i * paso for i in range(num_muestras)]
+    return tiempo
+
+def generar_senal(json_string, tiempo):
     data = json.loads(json_string)
-    # Aseguramos que los valores de la señal sean flotantes
-    return [float(value) for value in data["signal"].split(",")]
+    amplitudes = data["amplitud"]
+    frecuencias = data["frecuencias"]
+    
+    senal = [sum(A * math.sin(2 * math.pi * f * t) for A, f in zip(amplitudes, frecuencias)) for t in tiempo]
+    return senal
 
-def Transformada_discreta_directa(signal):
+def Transformada_discreta_directa(signal, fs):
     N = len(signal)
-    coeficientes = []  
-    magnitudft = []
+    coeficientes = []
+    frecuencias = [(k * fs / N) if k < N//2 else ((k - N) * fs / N) for k in range(N)]
+    
     for k in range(N):
         real = sum(signal[n] * math.cos(-2 * math.pi * k * n / N) for n in range(N))
         imag = sum(signal[n] * math.sin(-2 * math.pi * k * n / N) for n in range(N))
-        coeficientes.append((real, imag))  # Guardamos los coeficientes complejos de fourier en la lista
-        magnitude = math.sqrt(real**2 + imag**2)  # Calculamos la magnitud de la frecuencia utulizando los coeficientes
-        magnitudft.append(magnitude) # Guardamos las magnitudes calculadas en la lista
-    return coeficientes, magnitudft
+        coeficientes.append((real, imag))  
+    
+    return coeficientes, frecuencias
 
 def Transformada_inversa(coeficientes):
-   #Hacemos la sumatoria con los coeficientes calculados en la funcion de transformada directa
     N = len(coeficientes)
     reconstruccion = []
     for n in range(N):
-        sum_real = sum(coeff[0] * math.cos(2 * math.pi * k * n / N) - coeff[1] * math.sin(2 * math.pi * k * n / N) for k, coeff in enumerate(coefficients))
-        reconstruccion.append(sum_real / N)  # Dividir por N para la normalización
+        sum_real = sum(coeff[0] * math.cos(2 * math.pi * k * n / N) - coeff[1] * math.sin(2 * math.pi * k * n / N) for k, coeff in enumerate(coeficientes))
+        reconstruccion.append(sum_real / N)
     return reconstruccion
 
 def lambda_handler(event, context):
-    json_string = event["json_string"]
+    print(event)
+    body = json.loads(event["body"])
+    print(body)
+
+    json_string = body["json_string"]
+    filtro_tipo = body["filtro"]
+    parametros_filtro = body["parametros"]
+    fs = body["fs"]
+    duracion = body["duración"]
     
-    signal = carga_signal_json(json_string)
-    coeficientes, magnitud = Transformada_discreta_directa(signal)
-    reconstruccion = Transformada_inversa(coeficientes)  
+   
+    tiempo = generar_tiempo(fs, duracion)
+    signal = generar_senal(json_string, tiempo)
+    coeficientes, frecuencias = Transformada_discreta_directa(signal, fs)
+
+    
+    response = client.invoke(
+        FunctionName='FILTROS',
+        InvocationType='RequestResponse',
+        Payload=json.dumps({
+            "coeficientes": coeficientes,
+            "frecuencias": frecuencias,
+            "fs": fs,
+            "filtro": filtro_tipo,
+            "parametros": parametros_filtro
+        })
+    )
+    
+    response_payload = json.load(response['Payload'])
+
+    # Validación de la clave "coef_filtrados"
+    if "coef_filtrados" not in response_payload:
+        return {"error": "La respuesta de FILTROS no contiene 'coef_filtrados'", "response_payload": response_payload}
+
+    coef_filtrados = response_payload["coef_filtrados"]
+    reconstruccion = Transformada_inversa(coef_filtrados)
+
     return {
-        "MAGNITUD": magnitud,
-        "COEFICIENTES": coeficientes,
-        "RECONSTRUCCIÓN": reconstruccion  # Señal reconstruida
-        #"signal": signal
-
+        "SIGNAL_ORIGINAL": signal,
+        "COEFICIENTES_ORIGINALES": coeficientes,
+        "FRECUENCIAS": frecuencias,
+        "COEFICIENTES_FILTRADOS": coef_filtrados,
+        "SIGNAL_FILTRADA": reconstruccion
     }
-
-
